d69ab575d3421f32caac04d602b3f3f76104210d
ablazleon <ablazleon@gmail.com>
2020-07-03 17:28:41 +0000

    Rubric compliant version

--- etl.py
+++ etl.py
@@ -3,41 +3,49 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+from pandas import DataFrame
 
 def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath,lines='True')
 
     # insert song record
-    song_data = 
+    df_song = DataFrame(df, columns=['song_id','title','artist_id','year','duration'])
+    song_data = df_song.values.tolist()[0]
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    df_artist = DataFrame(df, columns=['artist_id','artist_name','artist_location','artist_latitude','artist_longitude'])
+    artist_data = df_artist.values.tolist()[0]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath,lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page=='NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'],unit='ms')
     
-    # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    # insert time data records    
+    hour =t.dt.hour
+    day = t.dt.day
+    week = t.dt.week
+    month = t.dt.month
+    year = t.dt.year
+    weekday =t.dt.dayofweek
+    time_data = (t,hour,day,week,month,year,weekday)
+    column_labels = ('start_time','hour','day','week','month','year','weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels,time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = DataFrame(df, columns=['userId','firstName','lastName','gender','level'])
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +64,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (pd.to_datetime(row.ts,unit='ms'),row.userId,row.level,songid,artistid,row.sessionId,row.location,row.userAgent)               
         cur.execute(songplay_table_insert, songplay_data)
 
 
